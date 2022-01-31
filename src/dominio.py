dcd292db3f59a605ec91073f47108f5106e3fff2
Carlos Eduardo <caduarruda49@gmail.como>
2022-01-31 19:34:57 -0300

    dia31_5

--- src/dominio.py
+++ src/dominio.py
@@ -1,5 +1,3 @@
-import sys
-
 class Usuario:
 
     def __init__(self, nome, carteira):
@@ -7,20 +5,24 @@ class Usuario:
         self.__carteira = carteira
 
     def propoe_lance(self, leilao, valor):
+        if self._valor_e_valido(valor):
+            raise ValueError('Não pode propor um lance con valor maior que o valor da carteira')
+
         lance = Lance(self, valor)
         leilao.propoe(lance)
-        
+
         self.__carteira -= valor
 
-    # acessa o nome
     @property
     def nome(self):
         return self.__nome
-    
+
     @property
     def carteira(self):
         return self.__carteira
 
+    def _valor_e_valido(self, valor):
+        return valor > self.__carteira
 class Lance:
 
     def __init__(self, usuario, valor):
@@ -33,8 +35,8 @@ class Leilao:
     def __init__(self, descricao):
         self.descricao = descricao
         self.__lances = []
-        self.maior_lance = sys.float_info.min
-        self.menor_lance = sys.float_info.max
+        self.maior_lance = 0.0
+        self.menor_lance = 0.0
 
     # acessa os lances
     @property
@@ -42,12 +44,23 @@ class Leilao:
         return self.__lances[:]  # copia rasa [:]
 
     def propoe(self, lance: Lance):
-        if not self.__lances or self.__lances[-1].usuario != lance.usuario and lance.valor > self.__lances[-1].valor:
-            if lance.valor > self.maior_lance:
-                self.maior_lance = lance.valor
-            if lance.valor < self.menor_lance:
+        if self._lance_valido(lance):
+            if not self._tem_lances():
                 self.menor_lance = lance.valor
-
+            self.maior_lance = lance.valor
             self.__lances.append(lance)
         else:
             raise ValueError('Erro ao propor lance')
+
+    def _tem_lances(self):
+        return self.__lances
+
+    def _usuarios_diferentes(self, lance):
+        return self.__lances[-1].usuario != lance.usuario
+
+    def _valor_maior_que_lance_anterios(self, lance):
+        return lance.valor > self.__lances[-1].valor
+    
+    def _lance_valido(self, lance):
+        return not self._tem_lances() or (self._usuarios_diferentes(lance) and
+                                         self._valor_maior_que_lance_anterios(lance))
--- tests/test_usuario.py
+++ tests/test_usuario.py
@@ -1,10 +1,29 @@
 from src.dominio import Leilao, Usuario
+import pytest
 
-def test_deve_subtrair_valor_da_carteira_do_usuario_quando_este_propor_um_lance():
-    vini = Usuario('Vini', 100.0)
+@pytest.fixture
+def vini():
+    return Usuario('Vini', 100.0)
 
-    leilao = Leilao('Celular')
+@pytest.fixture
+def leilao():
+    return Leilao('Celular')
 
-    vini.propoe.lance(Leilao, 50.0)
+def test_deve_subtrair_valor_da_carteira_do_usuario_quando_este_propor_um_lance(vini, leilao):
+    vini.propoe_lance(leilao, 50.0)
 
     assert vini.carteira == 50.0
+
+def test_deve_permitir_propor_lance_quando_o_valor_e_menor_que_o_valor_da_carteria(vini, leilao):
+    vini.propoe_lance(leilao, 5.0)
+
+    assert vini.carteira == 95
+
+def teste_deve_permitir_propor_lance_quando_o_valor_e_igual_ao_valor_da_carteira(vini, leilao):
+    vini.propoe_lance(leilao, 100.0)
+
+    assert vini.carteira == 0.0
+
+def test_onde_nao_deve_propor_lance_com_valor_maior_que_o_da_carteira(vini, leilao):
+    with pytest.raises(ValueError):
+        vini.propoe_lance(leilao, 200.0)
